027497656fbac5940b3c97ca5d3c5a7c46e512b3
vitortissott <vitor.tissott@escola.pr.gov.br>
2026-06-07 20:06:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Aqui é o código responsavel por iniciar a tradução do site conectando ao google tradutor
 function iniciarTradutor() {
   new google.translate.TranslateElement(
     { pageLanguage: 'pt' },
     'google_tradutior'
   );
 }
-
+// Aqui é o código responsavel por alterar o didioma do site
 function alternarIdioma() {
   const dropdown = document.getElementById("idiomaDropdown");
   dropdown.classList.toggle("mostrar");
@@ -19,10 +20,35 @@ window.onclick = function(evento) {
   }
 };
 
+//Aqui executa a tradução total do site mudando para qualquer idioma do google tradutor
 function googleTranslateElementInit() {
   iniciarTradutor();
 }
 
+//Essa é a função responsavel por aumentar e diminuir as fontes no site
+let tamanhoFonte = 100;
+
+function alterarFonte(valor){
+
+    tamanhoFonte += valor * 10;
+
+    if(tamanhoFonte < 80) tamanhoFonte = 80;
+    if(tamanhoFonte > 180) tamanhoFonte = 180;
+
+    document.body.style.fontSize = tamanhoFonte + "%";
+
+    document.querySelectorAll(
+        '.tela, .tela_2, .tela_3, .tela_4, .celular_tela, .tela_tv, .tela_deitado, .tela_deitado_2, .conteudo'
+    ).forEach(elemento => {
+        elemento.style.fontSize = tamanhoFonte + '%';
+    });
+}
+
+function alternarTema(){
+    document.body.classList.toggle("modo-escuro");
+}
+
+//Aqui é a constante das perguntas do quiz no final do site
 const perguntas = [
   {
     pergunta: "1. Qual dessas abelhas não possui ferrão funcional?",
@@ -51,10 +77,12 @@ const perguntas = [
   }
 ];
 
+//As variáveis usadas no quiz
 let perguntaAtual = 0;
 let pontuacao = 0;
 let respostasUsuario = new Array(5).fill(null);
 
+//Aqui é a criação das perguntas no código HTML e CSS
 function carregarPergunta() {
   const p = perguntas[perguntaAtual];
 
@@ -84,6 +112,7 @@ function carregarPergunta() {
   atualizarBotoes();
 }
 
+//Está é a função responsavel por ir para a proxima pergunta
 function proximaPergunta() {
   if (respostasUsuario[perguntaAtual] === null) {
     alert("Selecione uma resposta!");
@@ -105,6 +134,7 @@ function perguntaAnterior() {
   }
 }
 
+//Ao finallizar o quiz, esta função mostra o numero de acertos que o usuiário teve no quiz
 function mostrarResultado() {
   pontuacao = 0;
 
@@ -120,6 +150,7 @@ function mostrarResultado() {
     "Você acertou " + pontuacao + " de " + perguntas.length;
 }
 
+//Caso o usuário queira reiniciar o quiz foi eaborado esta função
 function reiniciarQuiz() {
   perguntaAtual = 0;
   pontuacao = 0;
